553b126f2ba5cc6572eaf454a721cfe7fdb79e28
forcecore <jdj@arin>
2011-04-10 23:38:39 +0900

    Corrected autogen.sh (was in wrong order)

--- autogen.sh
+++ autogen.sh
@@ -9,8 +9,11 @@ aclocal || exit 1
 echo "autoheader..."
 autoheader || exit 1
 
-echo "automake..."
-automake -a -c || exit 1
-
 echo "autoconf..."
 autoconf || exit 1
+
+echo "libtoolize..."
+libtoolize || exit 1
+
+echo "automake..."
+automake -a -c || exit 1
